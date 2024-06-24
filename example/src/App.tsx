9fdc3cc60cdad9f9daab404a1823e197b663806b
Umar Zakari <umarfarouqft@gmail.com>
2024-06-24 09:33:31 +0100

    chore: setting up npm

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,26 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-juicelife';
+import { StyleSheet, View } from 'react-native';
+import { Workout } from 'react-native-juicelife';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  // const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  // React.useEffect(() => {
+  //   multiply(3, 7).then(setResult);
+  // }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Workout apiKey="1" workoutId="1" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,36 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React from 'react';
+import { Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+type WorkoutProps = {
+  workoutId: string;
+  apiKey: string;
+  metaData?: object;
+  callback?: string;
+};
+
+export function Workout({
+  workoutId,
+  apiKey,
+  metaData,
+  ...props
+}: WorkoutProps) {
+  // const [loaded, setLoaded] = React.useState(false);
+
+  if (!workoutId || !apiKey) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Workout ID & API Key is required</Text>
+      </View>
+    );
+  }
+  return (
+    <WebView
+      source={{
+        uri: `https://juicelife-react.vercel.app/?workoutId=${workoutId}&apiKey=${apiKey}&metaData=${JSON.stringify(metaData)}&callback=${props.callback}`,
+      }}
+      style={{ flex: 1 }}
+      // onLoad={() => setLoaded(true)}
+    />
+  );
 }
